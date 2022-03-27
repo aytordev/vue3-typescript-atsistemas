eca5678fb5f70a29d5467d7110bc7e1222a6cd25
Aytor Vicente Martínez <avicentemart@outlook.com>
2022-03-27 16:14:47 +0200

    Add template static check by vue-tsc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
     },
   },
   plugins: [vue()],
-});
+})

f5d22f9ee4fd0070b252d0e8063d3067e798ae65
Aytor Vicente Martínez <avicentemart@outlook.com>
2022-03-27 19:05:05 +0200

    Implement vue-router using a loader to get route modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import App from '@/App.vue';
+import router from '@/router';
 import store from '@/store';
 import { createApp } from 'vue';
 
 const application = createApp(App);
 
+application.use(router);
 application.use(store);
 application.mount('#app');
---
+++ src/router/routes/modules/films.ts
@@ -0,0 +1,6 @@
+export default {
+  path: '/films',
+  name: 'films',
+  component: () => import('@/views/films/FilmsView.vue'),
+  default: true,
+};

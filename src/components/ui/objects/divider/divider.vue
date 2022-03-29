c4165d6185da9c3c98f0cb46dc9f959f01c11336
Aytor Vicente Martínez <avicentemart@outlook.com>
2022-03-30 01:02:46 +0200

    Fix some styles & implements osme UI components like divs, list or containers

---
+++ src/components/ui/objects/divider/divider.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { computed, defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'Divider',
+    props: { vertical: { type: Boolean } },
+    setup(props) {
+      const classes = computed(() => [
+        {
+          'divider--horizontal': !props.vertical,
+          'divider--vertical': props.vertical,
+        },
+      ]);
+      return { classes };
+    },
+  });
+</script>
+
+<template>
+  <div class="divider" :class="classes" role="separator" />
+</template>
+
+<style lang="scss" scoped>
+  .divider {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    background-color: var(--text-color);
+
+    &--horizontal {
+      display: block;
+      clear: both;
+      width: 100%;
+      min-width: 100%;
+      height: 1px;
+      margin: 0;
+    }
+
+    &--vertical {
+      display: inline-block;
+      width: 1px;
+      margin: 0 0.5rem;
+    }
+  }
+</style>
---
+++ src/components/ui/objects/list/list.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { defineComponent, h } from 'vue';
+
+  export default defineComponent({
+    props: {
+      horizontal: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    setup(props, context) {
+      return () =>
+        h(
+          'ul',
+          {
+            class: [
+              'list',
+              props.horizontal && 'list--horizontal',
+              !props.horizontal && 'list--vertical',
+            ],
+          },
+          context.slots
+        );
+    },
+  });
+</script>
+<style lang="scss">
+  @use '@/src/assets/styles/settings/variables';
+  .list {
+    margin: variables.$padding-0;
+    padding: variables.$margin-0;
+    list-style: none;
+
+    &--horizontal {
+      display: flex;
+      flex-wrap: nowrap;
+      gap: 1rem;
+      align-items: center;
+    }
+
+    &--vertical {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+</style>

8cacf3964da31c87f08e9f2b5003568eb358d129
Aytor Vicente Martínez <avicentemart@outlook.com>
2022-03-30 00:33:05 +0200

    Implement scss architecture with some mixins, variables & tools

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
-<script setup lang="ts">
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  import HelloWorld from '@/components/HelloWorld.vue';
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'App',
+  });
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <router-view></router-view>
 </template>
 
-<style>
+<style lang="scss">
+  @use './src/assets/styles/settings/variables.scss';
   #app {
-    margin-top: 60px;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    text-align: center;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+    height: calc(variables.$height-100 - variables.$space-48);
   }
 </style>
